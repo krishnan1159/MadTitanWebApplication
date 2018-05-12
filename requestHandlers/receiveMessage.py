e41fcadebedcdf83d76ee381857b74c14b63ceb6
Krishnan Marimuthu <marimutk@amazon.com>
2018-05-12 19:16:08 +0530

    Adding logs for receive message

--- requestHandlers/receiveMessage.py
+++ requestHandlers/receiveMessage.py
@@ -1,7 +1,9 @@
 from flask.views import MethodView
 from flask import request
 import json
+import logging
 
 class ReceiveMessageHander(MethodView):
 	def post(self):
+		logging.error('++++ I told you so' + request.get_data(as_text=True))
 		return "Complete Request :: " + request.get_data(as_text=True)
